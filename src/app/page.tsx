61a1af30726be6688c0415ca59998725cb4a06dd
Ranzit-Dangal <ranzitdangal@gmail.com>
2025-07-18 14:50:41 +0545

    button

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,13 @@
+import Link from "next/link";
+
 export default function Home() {
-  return <div>homepage</div>;
+  return (
+    <div className="justify-center items-center flex flex-col h-screen bg-black">
+      <Link href={"/dashboard/admin"}>
+        <button className="px-8 py-3 border-2 border-black rounded-full bg-red-800 text-slate-200">
+          Press........ when you are ready!!!
+        </button>
+      </Link>
+    </div>
+  );
 }
