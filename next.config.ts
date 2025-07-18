39ff322768781468a5969c4613f158f4d31ceb00
Ranzit-Dangal <ranzitdangal@gmail.com>
2025-07-18 15:32:45 +0545

    preproduction/esling disabled

--- next.config.ts
+++ next.config.ts
@@ -5,8 +5,13 @@ const nextConfig: NextConfig = {
     remotePatterns:[
       {hostname:"images.pexels.com"}
     ]
-  }
+  },
   /* config options here */
+    eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  }
 };
 
 export default nextConfig;
